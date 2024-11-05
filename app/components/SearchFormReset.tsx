e85003e9155b1b209c8477f6c5e916cdeb5e20b6
Sandica Cristian <cristian.sandica@skywindgroup.com>
2024-11-05 15:56:31 +0200

    search logic

---
+++ app/components/SearchFormReset.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { Link } from 'lucide-react';
+import React from 'react'
+
+const SearchFormReset = () => {
+    const reset = () => {
+        const form = document.querySelector('.search-form') as HTMLFormElement;
+
+        if (form) form.reset();
+    }
+    return (
+        <button type="reset" onClick={reset}>
+            <Link href="/" className='search-btn text-white'>X</Link>
+        </button>
+    )
+}
+
+export default SearchFormReset
